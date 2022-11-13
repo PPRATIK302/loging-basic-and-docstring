5c46199727f8e90cb4379c42dce0f9d87e122d92
91854 <anjali.choudhary438@gmail.com>
2022-11-13 17:59:36 +0530

    or changes

--- or.py
+++ or.py
@@ -1,9 +1,25 @@
 from utils.all_utils import prepare_data,save_plot
 import pandas as pd
 from utils.model import Perceptron
+import logging
+import os
+
+gate = "or"
+log_dir = "logs"
+os.mkdir(log_dir)
+logging.basicConfig(
+    filename=os.path.join(log_dir,"running_logs.log"),
+    level=logging.INFO,
+    format='[%(asctime)s: %(levelname)s: %(module)s]:%(message)s',
+    filemode='a'
+
+
+)
 
 def main(data,modelName,plotName,eta,epochs):
     df_OR = pd.DataFrame(OR)
+    gate 
+    logging.info(f"this is the raw dataset:{df_OR}")
     X, y = prepare_data(df_OR)
 
     
@@ -26,7 +42,13 @@ if __name__ == "__main__":
 }
     ETA = 0.3
     EPOCHS = 10
-    main(data=OR,modelName="or.model",plotName="or.png",eta=ETA,epochs=EPOCHS)
+    try:
+        logging.info(f">>>>starting trainin>>>>> for {gate}")
+        main(data=OR,modelName="or.model",plotName="or.png",eta=ETA,epochs=EPOCHS)
+        logging.info(f"<<<<done training>>>>>>>> for {gate}")
+    except Exception as e:
+        logging.exception(e)
+        raise e
 
 
 
